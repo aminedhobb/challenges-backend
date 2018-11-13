4fff04e38de0b8f380037dc11d3feb7f6cda991e
aminedhobb <amine.dhobb@gmail.com>
2018-11-13 21:32:56 +0100

    level 5

---
+++ level5/main.rb
@@ -0,0 +1,35 @@
+require 'json'
+require 'date'
+require './lib/price_by_time'
+require './services/calculate_commissions_service'
+
+input_path = './data/input.json'
+serialized_input = File.read(input_path)
+input = JSON.parse(serialized_input)
+output = []
+
+input['rentals'].each do |rental|
+
+  car = input['cars'].find { |c| c['id'] == rental['car_id'] }
+  options = input['options'].select { |o| o['rental_id'] == rental['id'] }
+  options.map! { |option| option['type'] }
+
+  number_of_days = (Date.parse(rental['end_date']) - Date.parse(rental['start_date'])).to_i + 1
+  price = price_by_time(number_of_days, car['price_per_day']).round +
+    rental['distance'] * car['price_per_km']
+
+  calculate_commissions_service = CalculateCommissionsService.new(number_of_days, price, options)
+  calculate_commissions_service.call
+  actions = calculate_commissions_service.actions
+
+  output << {
+    id: rental['id'],
+    options: options,
+    actions: actions
+  }
+
+end
+
+File.open('./data/output.json', 'w') do |f|
+  f.write(JSON.pretty_generate(rentals: output))
+end
---
+++ level5/services/calculate_commissions_service.rb
@@ -0,0 +1,68 @@
+# when called, creates an instance variable as an array containing all the actions and the amounts
+# for the driver, the owner, the insurance, the assistance and drivy
+class CalculateCommissionsService
+
+  attr_reader :actions
+
+  def initialize(number_of_days, price, options)
+    @price = price
+    @number_of_days = number_of_days
+    @options = options
+    @additional_owner_fee = 0
+    @additional_drivy_fee = 0
+  end
+
+  def call
+    insurance_fee = (@price * 0.3 * 0.5).round
+    assistance_fee = @number_of_days * 100
+    drivy_fee = @price * 0.3 - (insurance_fee + assistance_fee)
+
+    check_for_options
+    render_actions(insurance_fee, assistance_fee, drivy_fee)
+  end
+
+  private
+
+  def check_for_options
+    @options.each do |option|
+      case option
+      when 'gps'
+        @additional_owner_fee += 500 * @number_of_days
+      when 'baby_seat'
+        @additional_owner_fee += 200 * @number_of_days
+      when 'additional_insurance'
+        @additional_drivy_fee += 1000 * @number_of_days
+      end
+    end
+  end
+
+  def render_actions(insurance_fee, assistance_fee, drivy_fee)
+    @actions = [
+      {
+        who: 'driver',
+        type: 'debit',
+        amount: @price + @additional_drivy_fee + @additional_owner_fee
+      },
+      {
+        who: 'owner',
+        type: 'credit',
+        amount: (@price * 0.7).round + @additional_owner_fee
+      },
+      {
+        who: 'insurance',
+        type: 'credit',
+        amount: insurance_fee
+      },
+      {
+        who: 'assistance',
+        type: 'credit',
+        amount: assistance_fee
+      },
+      {
+        who: 'drivy',
+        type: 'credit',
+        amount: drivy_fee.round + @additional_drivy_fee
+      }
+    ]
+  end
+end
