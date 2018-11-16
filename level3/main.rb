d1286738b92ef2b0f36dc4090c98b2d124efcdc9
aminedhobb <amine.dhobb@gmail.com>
2018-11-16 15:21:58 +0100

    added spec to level 3

--- level3/main.rb
+++ level3/main.rb
@@ -8,20 +8,24 @@ input = JSON.parse(serialized_input)
 output = []
 
 input['rentals'].each do |rental|
+  
   car = input['cars'].find { |c| c['id'] == rental['car_id'] }
+
   number_of_days = (Date.parse(rental['end_date']) - Date.parse(rental['start_date'])).to_i + 1
   price = price_by_time(number_of_days, car['price_per_day']).round +
     rental['distance'] * car['price_per_km']
-  insurance_fee = (price * 0.3 * 0.5).round
+
+  insurance_fee = (price * 0.3 * 0.5).floor
   assistance_fee = number_of_days * 100
   drivy_fee = price * 0.3 - (insurance_fee + assistance_fee)
+
   output << { 
     id: rental['id'], 
     price: price, 
-    commission: { 
+    commission: {
       insurance_fee: insurance_fee,
-      assistance_fee: assistance_fee, 
-      drivy_fee: drivy_fee.to_i
+      assistance_fee: assistance_fee,
+      drivy_fee: drivy_fee.floor
     }
   }
 end
